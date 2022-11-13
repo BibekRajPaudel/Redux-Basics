393328d1d48868bb0ddc79794892875658e15b48
BibekRajPaudel <paudelrajbibek@gmail.com>
2022-11-13 14:09:31 +0545

    redux basics setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,10 @@
-import { useSelector, useDispatch } from 'react-redux';
-import './App.css';
-import { increment } from './Action/index';
+import Counter from "./features/counter/counter";
 
 function App() {
-  const counter = useSelector(state => state.counter )
-  const isLogged = useSelector(state => state.isLogged )
-  const dispatch = useDispatch()
   return (
-    <div className="App">
-      <h1>counter {counter}</h1>
-      <button onClick={()=>dispatch(increment())} ></button>
-      {isLogged ? <h3> Valueable information</h3> :""}
-     HEllooooo
-    </div>
+    <main className="App">
+      <Counter />
+    </main>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {createStore} from "redux"
-import allReducers from "./Reducers/index"
-import { composeWithDevTools } from "redux-devtools-extension";
+import { store } from './app/store';
 import { Provider } from 'react-redux';
 
-
-const store = createStore(allReducers, composeWithDevTools())
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider store={store}>
-    <App />
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
     </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
